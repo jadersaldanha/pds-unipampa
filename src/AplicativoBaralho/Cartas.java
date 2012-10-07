6b9408af4f248f3489b1e42a3c313f493f71f7cb
Jader <saldanha.jader@gmail.com>
2012-10-07 18:12:37 -0300

    Adicionando no cabeçalho texto de licença

--- src/AplicativoBaralho/Cartas.java
+++ src/AplicativoBaralho/Cartas.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2012 Jader de Freitas Saldanha
+ * 
+ * Este arquivo e parte do programa AplicativoBaralho
+ * 
+ * AplicativoBaralho e um software livre; voce pode redistribui-lo e/ou modifica-lo dentro dos termos da Licenca Publica Geral Menor GNU 
+ * como publicada pela Fundacao do Software Livre (FSF); na versao 2 da Licenca, ou (na sua opiniao) qualquer versao.
+ * 
+ *  Este programa e distribuido na esperanca que possa ser util, mas SEM NENHUMA GARANTIA; sem uma garantia implicita de ADEQUACAO a qualquer
+ *  MERCADO ou APLICACAO EM PARTICULAR. Veja a Licenca Publica Geral Menor GNU para maiores detalhes.
+ *  
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral Menor GNU junto com este programa, se nao, escreva para a Fundacao do Software
+ *  Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+
 package AplicativoBaralho;
 
 
